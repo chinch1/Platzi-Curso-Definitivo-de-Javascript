11d10ce3560e8ff3b6bbfc34013432b417a2c017
Nicola Bitetto <nicolaandresr@gmail.com>
2019-12-04 17:35:57 -0400

    devDependencies
    Added gulp and gulp-saas to devDependencies for good practices

--- Platzigram/server.js
+++ Platzigram/server.js
@@ -3,7 +3,7 @@ var app= express();
 
 app.get('/', function (req, res) {
   res.send('Hola Mundo!');
-})
+});
 
 app.listen(3000, function (err) {
   if (err) {
@@ -11,9 +11,7 @@ app.listen(3000, function (err) {
   }
   
   console.log('Kratosgram escuchando en el puerto 3000');
-})
-
-
+});
 
 // Express.JS es una libreria que permite crear microframeworks web, para hacer servidores web de forma sencilla. 
 // Lo corrimos con NPM, el popular gestor de paquetes (como Bower.js y duo.js).
